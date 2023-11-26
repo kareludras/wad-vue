712f9f21812968bc79bbefe58eda94ff5f675c94
kareludras <udras.karel@gmail.com>
2023-11-26 18:07:05 +0200

    added css style but still quite broken

--- wad-vue.js/src/App.vue
+++ wad-vue.js/src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <div class="headerImage"> <!-- Assuming you have a logo or image for the header -->
+      <!-- logo/headerimage here -->
+    </div>
+
+    <nav>
+      <router-link to="/" class="nav-link">Home</router-link> |
+      <router-link to="/about" class="nav-link">About</router-link>
+    </nav>
+
+    <!-- dropdown components -->
+
+  </header>
+
+  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  background-color: #fff7eb;
+}
+
+header {
+  background-color: #288b10;
+  color: #fff;
+  padding: 10px 0;
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+  height: 25px;
+  position: sticky;
+  top: 0;
+  margin-bottom: 5px;
 }
 
 nav {
-  padding: 30px;
+  display: flex;
+  align-items: center;
 }
 
-nav a {
+.nav-link {
+  color: #fff;
+  text-decoration: none;
+  margin: 0px 5px;
   font-weight: bold;
-  color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.nav-link:hover {
+  text-decoration: underline;
+  background-color: #086313;
 }
+
+.headerImage {
+  display: flex;
+  align-items: center;
+  margin: 0px 20px;
+}
+
+.headerImage:hover {
+  cursor: pointer;
+}
+
+/* Add styles for the dropdown if needed */
+
+/* Add styles for the footer if needed */
 </style>
